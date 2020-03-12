7fe63d712e65f4a8086efd4311017a96e4795cee
Marthe-B <marthb14@uia.no>
2020-03-12 14:37:24 +0100

    Upload character movement
    Tilemap scene and Character scene

---
+++ HalfStalk/scripts/Node2D.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+var cross = load ("res://sprites/customMouse.png")
+var hotspot = Vector2(8,8)
+
+func _ready():
+	
+	Input.set_custom_mouse_cursor(cross, 0,hotspot)
+	#Input.set_custom_mouse_cursor(cross, Input.CURSOR_IBEAM)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
---
+++ HalfStalk/scripts/RayCast2D.gd
@@ -0,0 +1,11 @@
+extends RayCast2D
+
+#onready var raycast = $RayCast2D
+
+# not sure if this is the way to to it
+# This is for having the raycast2d follow the mouse
+# without rotating the character
+
+func _physics_process(delta):
+	var look_vec = get_global_mouse_position() - global_position
+	global_rotation = atan2(look_vec.y, look_vec.x)
